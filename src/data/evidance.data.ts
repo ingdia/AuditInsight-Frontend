9dd502fa97c14a7d09a98efd604da38a45b52a89
Diana BUYINZA <buyinzadiana@gmail.com>
2026-04-03 12:56:02 +0200

    feat: add initial mock Evidence data

---
+++ src/data/evidance.data.ts
@@ -0,0 +1,14 @@
+import {Evidance} from "@/types/evidance.types";
+
+export const evidanceData: Evidance[] = [
+
+    {
+        id: "DOC1",
+        name: "Invoice from ABC Corp",
+        category: "Invoice",
+        date: "03/15/2026",
+        amount: 12850,
+        transactionIds: ["TXN10542"],
+        status: "Verified",
+    },
+];
